154e8e1ddbd04a8343823776b3547c35cf623d38
Cliff_Tian <cliff_tian@hotmail.com>
2016-04-25 14:35:49 +0800

    Signed-off-by: Cliff_Tian <cliff_tian@hotmail.com>

--- src/com/day8/concurrency/MyRun.java
+++ src/com/day8/concurrency/MyRun.java
@@ -32,37 +32,3 @@ public class MyRun implements Runnable {
 	}
 
 }
-package com.day8.concurrency;
-
-public class MyRun implements Runnable {
-
-	public int i=0;
-	
-	public MyRun(int a){
-		i=a;
-	}
-	
-	@Override
-	public void run() {
-		int j = 10;
-		while (j > 0) {
-			System.out.println("hello" + i);
-			j = j - 1;
-		}
-
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Thread m1 = new Thread(new MyRun(1));
-		Thread m2 = new Thread(new MyRun(2));
-		m1.start();
-		m2.start();
-		
-	}
-
-}
