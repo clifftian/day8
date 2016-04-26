f07c4a529dc249c044ba6bef00d7409b93bcf702
Cliff_Tian <cliff_tian@hotmail.com>
2016-04-26 23:42:41 +0800

    Signed-off-by: Cliff_Tian <cliff_tian@hotmail.com>

---
+++ src/com/day8/mysql/TheFirstMySQLJDBC.java
@@ -0,0 +1,76 @@
+package com.day8.mysql;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TheFirstMySQLJDBC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/school?"
+					+ "user=ftian&password=Tz3338");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM teacher");
+
+			// or alternatively, if you don't know ahead of time that
+			// the query will be a SELECT...
+
+			/*
+			 * if (stmt.execute("SELECT foo FROM bar")) { rs =
+			 * stmt.getResultSet(); }
+			 */
+
+			while (rs.next()) {
+				System.out.println(rs.getString(1));
+				System.out.println(rs.getString(2));
+				System.out.println(rs.getString(3));
+			}
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		} finally {
+			// it is a good idea to release
+			// resources in a finally{} block
+			// in reverse-order of their creation
+			// if they are no-longer needed
+
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+
+				rs = null;
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+
+				stmt = null;
+			}
+		}
+
+	}
+
+}
