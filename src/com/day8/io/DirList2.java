bbe2ba53203b303e21e67a33d8ea109ab105afea
Cliff_Tian <cliff_tian@hotmail.com>
2016-07-27 22:17:11 +0800

    Jul-27

---
+++ src/com/day8/io/DirList2.java
@@ -0,0 +1,42 @@
+package com.day8.io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class DirList2 {
+
+	public static FilenameFilter filter (final String regex){
+		
+		return new FilenameFilter() {
+			private Pattern pattern=Pattern.compile(regex);
+			
+			public boolean accpet(File dir, String name){
+			return pattern.matcher(name).matches();
+			}
+		};
+		
+	}
+
+	public static void main(String[] args) {
+
+		File f = new File(".");
+		String list[];
+
+		System.out.println("start");
+		if (args.length == 0) {
+			list = f.list();
+		} else {
+			list = f.list(filter(args[0]));
+		}
+		Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+
+		for (String fname : list) {
+			System.out.println(fname);
+		}
+
+	}
+
+}
