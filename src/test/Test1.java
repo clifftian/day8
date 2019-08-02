96afe3dbd4cf56d14dda2dfa53993908a337200e
Cliff_Tian <cliff_tian@hotmail.com>
2019-08-02 17:28:23 +0800

    Aug-02-2019 Push

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -6,10 +6,34 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.CharBuffer;
+import java.util.Date;
 
 
 public class Test1 {
 	public static void main(String[] args) throws Exception  {
+		
+        java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm");
+        java.util.Date date = formatter.parse("2016-10-31 11:00");
+        if (date.compareTo(new java.util.Date()) < 0)
+        {
+        	//result="OK";
+        }else{
+        	//result="NOTOK"
+        }
+		Date dt=new Date();
+		
+		
+		int itest=0;
+		while (true){
+			
+			itest = itest + 1; System.out.println(itest);
+			java.lang.Thread.sleep(1000);
+			if (itest > 10){
+				break;
+				
+			}
+			
+		}
 		
 		File f = new File("f:/temp/ptc-tools.txt");
 		
